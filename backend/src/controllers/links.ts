c1c937a7ddb82567f6b5b41159d14813d22afadf
Leonardo Gomes <leogomes7@gmail.com>
2020-11-29 17:00:42 -0300

    Backend finalizado.

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,8 +1,7 @@
 import{Request, Response} from 'express';
 import {Link} from '../models/link';
+import linksRepository from '../models/linksRepository';
 
-const links: Link[] = [];
-let proxId = 1;
 
 function generateCode() {
     let text = '';
@@ -13,22 +12,39 @@ function generateCode() {
     return text;
 }
 
-function postLink(req: Request, res: Response) {
+async function postLink(req: Request, res: Response) {
     const link = req.body as Link;
-    link.id = proxId++;
     link.code = generateCode();
     link.hits = 0;
-    links.push(link);
+    const result = await linksRepository.add(link);
+    if (!result.id) return res.sendStatus(400);
+
+    link.id = result.id!;
+
     res.status(201).json(link);
 }
-
-function getLink(req: Request, res: Response) {
-    res.send('getLink');
+async function getLink(req: Request, res: Response) {
+    const code = req.params.code as string;
+    const link = await linksRepository.findByCode(code);
+    if(!link) {
+        res.sendStatus(404);
+      }  else {
+            res.json(link);
+    }
+    
 }
 
-function hitLink(req: Request, res: Response) {
-    res.send('hitLink');
+async function hitLink(req: Request, res: Response) {
+    const code = req.params.code as string;
+    const link = await linksRepository.hit(code)
+    if (!link)
+        res.sendStatus(404);
+        else {
+        res.json(link);
+        }
+    
 }
+
 
 export default {
     postLink,
